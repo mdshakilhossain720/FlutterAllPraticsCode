93e15803a8cd761624c48abfb5d508ca177e93b1
MD shakil Hossain <mdshakilhossain720@gmail.com>
2025-09-01 12:47:08 +0600

    dartadd

---
+++ dartcode/bin/frist/hello.dart
@@ -0,0 +1,36 @@
+void main() {
+  print("hello world");
+  print("hello world");
+  print("hello world");
+  var firstName = "John";
+  var lastName = "Doe";
+  print("Full name is $firstName $lastName");
+  int num1 = 10; //declaring number1
+  int num2 = 3; //declaring number2
+
+  // Calculation
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int mul = num1 * num2;
+  double div =
+      num1 / num2; // It is double because it outputs number with decimal.
+
+  // displaying the output
+  print("The sum is $sum");
+  print("The diff is $diff");
+  print("The mul is $mul");
+  print("The div is $div");
+
+  String name = "John";
+  String address = "USA";
+  num age = 20; // used to store any types of numbers
+  num height = 5.9;
+  bool isMarried = false;
+
+  // printing variables value
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+}
