32e171ded18c8c688e9dabe710518216bbb070a0
MD shakil Hossain <mdshakilhossain720@gmail.com>
2025-10-12 21:47:49 +0600

    token

---
+++ fluttercleanarchitecture/lib/core/data/remote/network_services.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:dio_http_formatter/dio_http_formatter.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'network_service_interceptor.dart';
+
+final networkServiceProvider = Provider<Dio>((ref) {
+  final options = BaseOptions(
+    baseUrl: baseURL,
+    connectTimeout: const Duration(seconds: 60),
+    receiveTimeout: const Duration(seconds: 60),
+    sendTimeout: const Duration(seconds: 60),
+  );
+
+  final dio = Dio(options);
+  final networkServiceInterceptor =
+      ref.watch(networkServiceInterceptorProvider(dio));
+
+  dio.interceptors.addAll([
+    HttpFormatter(),
+    networkServiceInterceptor,
+  ]);
+
+  return dio;
+});
